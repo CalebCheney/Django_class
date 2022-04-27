d48d9acd000cdfed129d01e30eb465935e9f8be1
Caleb Cheney <caleb_cheney1@baylor.edu>
2022-04-27 15:36:30 -0500

    4/27/22

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -6,5 +6,7 @@ app_name = "MainApp"
 
 urlpatterns = [
     path('', views.index, name='index'), #'' auto refers to home page address
-    path('topics', views.topics, name='topics'), #url/topics
+    path('topics', views.topics, name='topics'), #url/topics]
+    path('topics/<int:topic_id>/',views.topic, name='topic'), #url gets id added to it to go to differnt page
+    path('new_topic/', views.new_topic, name='new_topic'),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+
+from .forms import TopicForm
 from .models import Topic #adding topics needs to be imported
 
 # Create your views here.
@@ -13,3 +15,28 @@ def topics(request):
     context = {'topics':topics} #key value is what is used in html template, value is what you use in the view file
 
     return render(request, 'MainApp/topics.html', context)
+
+def topic(request, topic_id): #on exam topic_id has to be consistant with urls.py mainapp file
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added') #decending order
+
+    context = {'topic':topic,'entries':entries} #key and value
+
+    return render(request, 'MainApp/topic.html', context) #context is a dictionary object ! ON FINAL
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm #loads empty form
+    else: 
+        form = TopicForm(data=request.POST)
+
+        if form.is_valid():
+            new_topic = form.save()
+
+            return redirect('MainApp:topics')
+    context = {'form':form}
+    return render(request, 'MainApp/new_topic.html', context)
+
+
+
+
